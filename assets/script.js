87f3b831f6cb00a20d4783778636924d3330225e
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2022-10-19 13:32:46 -0500

    5 day forcast updates

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,37 @@
-function getCity(city){
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=bc44410118b10b4a172dcfb25048ec75`)
-        .then (function(response){
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data);
-            console.log(data[0].country);
-            console.log(data[0].name);
-            console.log(data[0].lat);
-            console.log(data[0].lon);
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            $('#city-name').text(data[0].name);
-            getWeather(lat,lon);
-        });
-        
-}
-
-function getWeather(lat,lon){
-    var link = 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=bc44410118b10b4a172dcfb25048ec75&units=imperial`)
+function getWeather(city){
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=48&appid=bc44410118b10b4a172dcfb25048ec75&units=imperial`)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
+            console.log(data);
+            $('#city-name').text(data.city.name);
+            $('#city-name').appendTo("hello");
             console.log(data.list[0].main.temp);
+            var weatherinfo = [data.list[0].dt_txt, data.list[0].main.temp,data.list[0].wind.speed, data.list[0].main.humidity];
+            for (var i = 0; i < 4; i++){
+                $('<li>' + `${weatherinfo[i]}` + '</li>').appendTo('#info');
+            }
+
+            //populate weather cards
+            var x = 2;
+            var weatherCards = $('.weather-day'); //get every card with a weather-day class
+            for (var i = 0; i < 5; i++){ //iterate 5 times because we have 5 cards
+                 //first card
+                $('<li>' + "Date: " + data.list[x].dt_txt + '</li>').appendTo('#' + weatherCards.eq(i).attr('id'));
+                $('<li>' + "Temp: " + data.list[x].main.temp + '</li>').appendTo('#' + weatherCards.eq(i).attr('id'));
+                $('<li>' + "Windspeed: " + data.list[x].wind.speed + '</li>').appendTo('#' + weatherCards.eq(i).attr('id'));
+                $('<li>' + "Humidity: " + data.list[x].main.humidity +"%" + '</li>').appendTo('#' + weatherCards.eq(i).attr('id'));
+                x += 8;
+            }
+            
+            
         })
 }
 
 
 $('#citySearch').on("click", function(){
     var cityInput = $('#userInput').val();
-    getCity(cityInput);
+    getWeather(cityInput);
 })
 
